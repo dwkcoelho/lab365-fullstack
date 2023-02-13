74f8beaf2b905807e3174618b438947faf2a36b2
dwkcoelho <dwkcoelho@gmail.com>
2023-02-13 20:50:37 -0300

    Learning about Constructors

---
+++ module I/week 3/person/Class/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace person.Class
+{
+    public class Person
+    {
+        public string name;
+        public int age;
+        public float height;
+
+        public Person() { } 
+        public Person(string name) 
+        {
+            this.name = name; 
+        }
+        public Person(int age) 
+        { 
+            this.age = age;
+        }
+        public Person(float height)
+        {
+            this.height = height;
+        }
+        public Person(string name,int age) { 
+            this.name = name;
+            this.age = age; 
+        }
+        public Person(string name, float height) 
+        { 
+            this.name = name;
+            this.height = height; 
+        }
+        public Person(int age, float height) { 
+            this.age = age; 
+            this.height = height; 
+        }
+        public Person(string name, int age, float height)  
+        {
+            this.name = name;
+            this.age = age;
+            this.height = height;
+        }
+    }
+}
---
+++ module I/week 3/person/person.cs
@@ -0,0 +1,27 @@
+﻿
+using person.Class;
+
+Person firstPerson = new Person();
+Console.WriteLine(firstPerson);
+
+Person secondPerson = new Person("Karol");
+Console.WriteLine($"The person's name is {secondPerson.name}");
+
+Person thirdPerson = new Person(26);
+Console.WriteLine($"The person's age is {thirdPerson.age}");
+
+Person fourthPerson = new Person(1.72f);
+Console.WriteLine($"The person's height is {fourthPerson.height}");
+
+Person fifthPerson = new Person("José", 22);
+Console.WriteLine($"The person's name is {fifthPerson.name} and your age is {fifthPerson.age}");
+
+Person sixthPerson = new Person("Willian", 1.80f);
+Console.WriteLine($"The person's name is {sixthPerson.name} and your height is {sixthPerson.height}");
+
+Person seventhPerson = new Person(28, 1.78f);
+Console.WriteLine($"The person's age is {seventhPerson.age} and your height is {seventhPerson.height}");
+
+Person eighthPerson = new Person("Diego", 26, 1.86f);
+Console.WriteLine($"The person's name is {eighthPerson.name}, your age is {eighthPerson.age} " +
+    $"and your height is {eighthPerson.height}");
