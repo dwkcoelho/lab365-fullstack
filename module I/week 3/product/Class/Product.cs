670bf35a319155254c22af45abdb55b622f40c09
dwkcoelho <dwkcoelho@gmail.com>
2023-02-14 22:09:35 -0300

    Learning about Constructors

---
+++ module I/week 3/product/Class/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace product.Class
+{
+    public class Product
+    {
+        public string name;
+        public double price;
+        public int amount;
+
+        public Product(string name, double price, int amount)
+        { 
+            this.name = name;
+            this.price = price;
+            this.amount = amount;   
+        }
+        public Product()
+        {
+            this.name = "Unnamed";
+            this.price = 0;
+            this.amount = 0;
+        }
+
+    }
+}
---
+++ module I/week 3/product/product.cs
@@ -0,0 +1,11 @@
+﻿using product.Class;
+
+Product firstProduct = new Product("Calculator", 5.75, 100);
+
+Product secondProduct = new Product();
+
+Console.WriteLine($"First product is named: {firstProduct.name}" +
+    $", price: {firstProduct.price}, amount: {firstProduct.amount}");
+
+Console.WriteLine($"Second product is named: {secondProduct.name}" +
+    $", price: {secondProduct.price}, amount: {secondProduct.amount}");
