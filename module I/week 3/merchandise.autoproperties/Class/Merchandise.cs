871083f2d50e31c38d243c94c717452d06f039f4
dwkcoelho <dwkcoelho@gmail.com>
2023-02-22 16:37:48 -0300

    Auto Properties

---
+++ module I/week 3/merchandise.autoproperties/Class/Merchandise.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace merchandise.Class
+{
+    public class Merchandise
+    {
+        public string Name { get; set; }
+        private float Priece { get; set; }
+        private int Amount { get; set; }
+        private string _store;
+        private DateTime? _expiration;
+        public Merchandise(string name, float priece, int amount)
+        {
+            Name = name;
+            Priece = priece;
+            Amount = amount;
+
+            _store = "Main Store";
+            _expiration = DateTime.Parse("2022-12-31");
+        }
+        public Merchandise()
+        {
+
+        }
+        public void DescribeMerchandise()
+        {
+           Console.WriteLine($"The product name is {Name}. \n" +
+                $"The product price is {Priece}. \n" +
+                $"and has the amount of {Amount}.");
+        }
+    }
+}
---
+++ module I/week 3/merchandise.autoproperties/Program.cs
@@ -0,0 +1,7 @@
+﻿using merchandise.Class;
+
+Merchandise firstMerchandise = new Merchandise("Television", 1500.00f, 50);
+firstMerchandise.DescribeMerchandise();
+Console.WriteLine(firstMerchandise.Name);
+firstMerchandise.Name = "";
+Console.WriteLine(firstMerchandise.Name);
