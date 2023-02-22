e6ce47f8d9849f476b81751bde620e988e62aacb
dwkcoelho <dwkcoelho@gmail.com>
2023-02-22 20:48:42 -0300

    Composition

---
+++ module I/week 4/client/Class/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client.Class
+{
+    public class Client
+    {
+        public string Name { get; set; }
+        public DateTime Birth { get; set; }
+        public Address Address { get; set; }
+
+        public Client(string name, DateTime birth, Address address)
+        {
+            Name = name;
+            Birth = birth;
+            Address = address;
+        }
+    }
+}
---
+++ module I/week 4/client/Program.cs
@@ -0,0 +1,11 @@
+﻿using client.Class;
+
+Client diegoAlegre = new Client("Paulo", DateTime.Parse("1996-05-07"), new Address()
+{
+    PublicPlace = "Your street",
+    Number = "XX",
+    State = "Your state",
+    City = "Your city",
+    Country = "Your country",
+    ZipCode = "XXXXXXXXX"
+}) ;
