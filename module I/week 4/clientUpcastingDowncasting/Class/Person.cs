c3fb171092f8bea3a0a4f64aa2870dd17ba52824
dwkcoelho <dwkcoelho@gmail.com>
2023-02-23 19:48:47 -0300

    Upcasting and Downcasting

---
+++ module I/week 4/clientUpcastingDowncasting/Class/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client.Class
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public DateTime Birth { get; set; }
+        public Address Address { get; set; }
+
+        public Person (string name, DateTime birth, Address adress)
+        {
+            Name = name;
+            Birth = birth;
+            Address = adress;
+        }
+        public virtual void HaveBirthday ()
+        {
+            Birth = Birth.AddYears (1);
+        }
+    }
+}
---
+++ module I/week 4/clientUpcastingDowncasting/Program.cs
@@ -0,0 +1,23 @@
+﻿using client.Class;
+
+//Upcasting
+Person diegoAlegre = new Client("Diego", DateTime.Parse("1996-02-02"), new Address()
+{
+    PublicPlace = "Your street",
+    Number = "XX",
+    State = "Your state",
+    City = "Your city",
+    Country = "Your country",
+    ZipCode = "XXXXXXXXX"
+}, "Apple");
+
+//Console.WriteLine(diegoAlegre.Store); - It dont work.
+
+//Downcasting
+
+Client clientDiegoAlegre = (Client)diegoAlegre;
+Console.WriteLine(clientDiegoAlegre.Store);
+
+
+
+
