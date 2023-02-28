724e0a7c9d396999eef5c55edd33384db224c33e
dwkcoelho <dwkcoelho@gmail.com>
2023-02-27 21:44:09 -0300

    From specific to general

---
+++ module I/week 5/exceptionDivideByZero/Program.cs
@@ -0,0 +1,21 @@
+﻿using exceptions.Class;
+
+int x = 2;
+int y = 0;
+
+try
+{
+    Console.WriteLine($"x / y is equal: {x/y}");
+}
+catch (DivideByZeroException exDivideByZero)
+{
+    Console.WriteLine( $"Explanation of the divide by zero error: { exDivideByZero.Message}" );
+}
+catch (ArithmeticException exArithmetic)
+{
+    Console.WriteLine($"Error Explanation: {exArithmetic.Message}");
+}
+catch (Exception exGeneral)
+{
+    Console.WriteLine($"Catch Error: {exGeneral.Message}");
+}
