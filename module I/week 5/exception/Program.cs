1c2ce6eaa3fb8c6e718916a921366c1784901f63
dwkcoelho <dwkcoelho@gmail.com>
2023-02-27 21:28:20 -0300

    Try Catch Finally

---
+++ module I/week 5/exception/Program.cs
@@ -0,0 +1,21 @@
+﻿using exceptions.Class;
+
+try
+{
+    // Handling data of another type.
+    Console.WriteLine("Write a number:");
+    int number = int.Parse(Console.ReadLine());
+    Console.WriteLine($"The result is {number} x 10 is: {number * 10}");
+}
+catch (FormatException ex)
+{
+    Console.WriteLine($"\nSpecific Error Caught: {ex.Message}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Caught Error: {ex}");
+}
+finally
+{
+    Console.WriteLine("\nYou need to write a number!");
+}
