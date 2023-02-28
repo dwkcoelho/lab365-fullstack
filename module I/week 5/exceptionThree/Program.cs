f3c8171b02622fe81dc8c0183b1fbc0f790a08bd
dwkcoelho <dwkcoelho@gmail.com>
2023-02-28 20:56:21 -0300

    Error Hadling

---
+++ module I/week 5/exceptionThree/Program.cs
@@ -0,0 +1,26 @@
+﻿try
+{
+    string text = "Teste";
+
+    int number = Convert.ToInt32(text);
+
+    int result = 100 / number;
+
+    Console.WriteLine($"The result of 100 divided by {number} is: {result}");
+}
+catch (DivideByZeroException exDivideByZero)
+{
+    Console.WriteLine($"Error Explanation: {exDivideByZero.Message}");
+}
+catch (FormatException exFormat)
+{
+    Console.WriteLine($"Error Explanation: {exFormat.Message}");
+}
+catch (Exception exGeneral)
+{
+    Console.WriteLine($"Catch Error: {exGeneral}");
+}
+finally
+{
+
+}
