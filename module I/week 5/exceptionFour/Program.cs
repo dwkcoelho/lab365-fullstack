2ecea62c769acfd4ff0c3f43cbeaa678aae9558b
dwkcoelho <dwkcoelho@gmail.com>
2023-02-28 21:14:04 -0300

    Null References Exception

---
+++ module I/week 5/exceptionFour/Program.cs
@@ -0,0 +1,20 @@
+﻿try
+{
+    string text = null; 
+    if (text.Length > 0)    
+    { 
+        Console.WriteLine($"Amount of characters: {text.Length}");
+    }
+}
+catch (NullReferenceException exNull)
+{
+    Console.WriteLine($"Error Explanation: {exNull.Message}.\nDo this to get out of this error!");
+}
+catch (Exception exGeneral)
+{
+    Console.WriteLine($"Catch Error: {exGeneral}");
+}
+finally
+{
+
+}
